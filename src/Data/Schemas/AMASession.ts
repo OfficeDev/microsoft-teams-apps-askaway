064097feae845a558d0c50d2c83ff1128f9fad07
Shayan Khalili Moghaddam <t-shakha@microsoft.com>
2020-06-12 17:13:07 -0400

    Added basic connection code for mongoose. Created AMASession Schema.

---
+++ src/Data/Schemas/AMASession.ts
@@ -0,0 +1,56 @@
+import * as mongoose from 'mongoose';
+import { boolean, string } from 'yargs';
+
+const AMASessionSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true,
+    },
+    description: {
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true,
+    },
+    active: {
+        type: boolean,
+        required: true,
+    },
+    host: {
+        type: mongoose.SchemaTypes.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    conversationId: {
+        type: string,
+        required: true,
+    },
+    tenantId: {
+        type: string,
+        required: true,
+    },
+    // The following id is the group chat or channel id based on the context of this session.
+    scope: {
+        scopeId: {
+            type: string,
+            required: true,
+        },
+        isChannel: {
+            type: boolean,
+            required: true,
+        },
+        required: true,
+    },
+    datetimeCreated: {
+        type: Date,
+        default: Date.now,
+    },
+    datetimeEnded: {
+        type: Date,
+        required: false,
+    },
+});
+
+export const AMASession = mongoose.model('AMASession', AMASessionSchema);
