76d1c66e8c73d48e3ed819b133721d0e511d7351
Shayan Khalili Moghaddam <t-shakha@microsoft.com>
2020-06-15 12:38:43 -0400

    Added import for AdaptiveCardBuilder in Controller.ts

--- src/Controller.ts
+++ src/Controller.ts
@@ -1,2 +1,3 @@
 // Middleman file to allow for communication between the bot, database, and adaptive card builder.
-import * as db from './Data/Database';
+import * as db from './Data/Database'; // For database calls
+import * as adaptiveCardBuilder from './AdaptiveCards/AdaptiveCardBuilder'; // To populate adaptive cards
