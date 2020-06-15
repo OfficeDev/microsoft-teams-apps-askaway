4efa7bc2e1d7a92468635f184e058dd94c588491
Shayan Khalili Moghaddam <t-shakha@microsoft.com>
2020-06-15 12:38:43 -0400

    Added import for AdaptiveCardBuilder in Controller.ts

--- src/AdaptiveCards/AdaptiveCardBuilder.ts
+++ src/AdaptiveCards/AdaptiveCardBuilder.ts
@@ -1,1 +1,2 @@
 // All the functions to populate the adaptive cards should go in here
+export default null; // This line is needed until we add some exports to this file, without it, ts complains that this file is not a module.
