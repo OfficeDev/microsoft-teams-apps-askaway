f1d9061696fed9f30ba5d1a61843c2de058e6b83
Shayan Khalili Moghaddam <t-shakha@microsoft.com>
2020-07-01 19:58:51 +0000

    Merged PR 920: Long questions don't get cut off in leaderboard. Changed "Created by" to "Ini...
    Addressed the following items from the bug bash:
    * Long questions get cut off in leaderboard.
    * "Created by ..." should be changed to "Initiated by ..." in master card
    
    Related work items: #10715

--- src/AdaptiveCards/MasterCard.ts
+++ src/AdaptiveCards/MasterCard.ts
@@ -49,7 +49,7 @@ export default {
         },
         {
             type: 'TextBlock',
-            text: 'Created by ${user}',
+            text: 'Initiated by ${user}',
             wrap: true,
             isSubtle: true,
         },
