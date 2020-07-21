1c7fa183fee3ba6275e542731f8c82cea8fe0021
Kavin Singh <t-kavsin@microsoft.com>
2020-07-21 17:35:02 +0000

    Merged PR 1040: Bug Fixes from latest bug bash
    * If  you start an AMA in a reply chain, the bot will now send the master card to the channel instead of a reply to a conversation.
    * After a master card update interval, the next time the master card can update is at least 5 seconds later and at most 10 seconds (configurable in `config.json` property `updateMasterCardPostDebounceTimeInterval`).
    * Fixed upvote spacing issue in master card.
    
    Related work items: #10956, #10959, #10960, #10962

--- src/AdaptiveCards/endAMAConfirmation.ts
+++ src/AdaptiveCards/endAMAConfirmation.ts
@@ -5,35 +5,36 @@ import { IAdaptiveCard } from 'adaptivecards';
 /**
  * Defines the template for the adaptive card used when confirming the ending of the AMA.
  */
-export default <IAdaptiveCard>{
-    $schema: 'http://adaptivecards.io/schemas/adaptive-card.json',
-    type: 'AdaptiveCard',
-    version: '1.0',
-    body: [
-        {
-            type: 'TextBlock',
-            text: 'Are you sure you want to stop gathering questions?',
-            size: 'large',
-        },
-    ],
-    actions: [
-        {
-            id: 'cancelEndAma',
-            type: 'Action.Submit',
-            title: 'Cancel',
-            data: {
-                amaSessionId: '${amaId}',
+export default () =>
+    <IAdaptiveCard>{
+        $schema: 'http://adaptivecards.io/schemas/adaptive-card.json',
+        type: 'AdaptiveCard',
+        version: '1.0',
+        body: [
+            {
+                type: 'TextBlock',
+                text: 'Are you sure you want to stop gathering questions?',
+                size: 'large',
+            },
+        ],
+        actions: [
+            {
                 id: 'cancelEndAma',
+                type: 'Action.Submit',
+                title: 'Cancel',
+                data: {
+                    amaSessionId: '${amaId}',
+                    id: 'cancelEndAma',
+                },
             },
-        },
-        {
-            id: 'submitEndAma',
-            type: 'Action.Submit',
-            title: 'End session',
-            data: {
-                amaSessionId: '${amaId}',
+            {
                 id: 'submitEndAma',
+                type: 'Action.Submit',
+                title: 'End session',
+                data: {
+                    amaSessionId: '${amaId}',
+                    id: 'submitEndAma',
+                },
             },
-        },
-    ],
-};
+        ],
+    };
