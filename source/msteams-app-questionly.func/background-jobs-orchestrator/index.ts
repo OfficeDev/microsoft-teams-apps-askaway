d5c9c786377a412c62b7a6fef7947f64268b44a3
Shraddha Sarda <sshraddha96@gmail.com>
2020-11-03 12:37:06 +0530

    Boilerplate code - Azure durable function architecture

---
+++ source/msteams-app-questionly.func/background-jobs-orchestrator/index.ts
@@ -0,0 +1,37 @@
+﻿/*
+ * This function is not intended to be invoked directly. Instead it will be
+ * triggered by an HTTP starter function.
+ *
+ * Before running this sample, please:
+ * - create a Durable activity function (default name is "Hello")
+ * - create a Durable HTTP starter function
+ * - run 'npm install durable-functions' from the wwwroot folder of your
+ *    function app in Kudu
+ */
+
+import * as df from "durable-functions";
+
+const orchestrator = df.orchestrator(function* (context) {
+  const outputs = [];
+
+  outputs.push(
+    yield context.df.callActivity("broadcast-message", "broadcast message")
+  );
+  outputs.push(
+    yield context.df.callActivity(
+      "send-notification-bubble",
+      "send notification bubble"
+    )
+  );
+  outputs.push(
+    yield context.df.callActivity(
+      "update-adaptive-card",
+      "update adaptive card"
+    )
+  );
+
+  // returns ["Activity - broadcast message!", "Activity - send notification bubble!", "Activity - update adaptive card!"]
+  return outputs;
+});
+
+export default orchestrator;
