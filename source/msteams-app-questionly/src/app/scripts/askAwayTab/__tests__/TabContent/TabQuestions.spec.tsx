bd18e68c249085d6d950e236aa94d2be6b5f921f
Vamshi Jillala <v-vajill@microsoft.com>
2021-02-07 21:31:51 -0800

    Added translation content t

--- source/msteams-app-questionly/src/app/scripts/askAwayTab/__tests__/TabContent/TabQuestions.spec.tsx
+++ source/msteams-app-questionly/src/app/scripts/askAwayTab/__tests__/TabContent/TabQuestions.spec.tsx
@@ -16,15 +16,16 @@ describe('TabQuestions Component', () => {
     const onClickAction = jest.fn();
     const activeSessionData = new Helper().createEmptyActiveSessionData();
     const theme = {} as ThemePrepared;
+    const t = jest.fn();
 
     it('should match the snapshot', () => {
-        const wrapper = shallow(<TabQuestions theme={theme} activeSessionData={activeSessionData} onClickAction={onClickAction} teamsTabContext={{ entityId: '', locale: '' }} />);
+        const wrapper = shallow(<TabQuestions t={t} theme={theme} activeSessionData={activeSessionData} onClickAction={onClickAction} teamsTabContext={{ entityId: '', locale: '' }} />);
 
         expect(enzymeToJson(wrapper)).toMatchSnapshot();
     });
 
     it('should render the tab', () => {
-        const component = shallow(<TabQuestions theme={theme} activeSessionData={activeSessionData} onClickAction={onClickAction} teamsTabContext={{ entityId: '', locale: '' }} />);
+        const component = shallow(<TabQuestions t={t} theme={theme} activeSessionData={activeSessionData} onClickAction={onClickAction} teamsTabContext={{ entityId: '', locale: '' }} />);
         console.log(component.debug());
 
         expect(component.find('div.question-container')).toBeTruthy();
