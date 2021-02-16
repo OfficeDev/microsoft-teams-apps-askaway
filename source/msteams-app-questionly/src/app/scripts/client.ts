bee32b249ff705ce79046983827d4bd8f220b8b4
Adrian Solis <Adrian.Solis@microsoft.com>
2021-02-16 20:48:45 +0000

    Merged PR 2353: Remove the unnecessary tab removal page
    Remove the unnecessary tab removal page
    
    [Bug 1542375](https://domoreexp.visualstudio.com/MSTeams/_workitems/edit/1542375): [Ask Away] Remove the tab remove page

--- source/msteams-app-questionly/src/app/scripts/client.ts
+++ source/msteams-app-questionly/src/app/scripts/client.ts
@@ -1,7 +1,6 @@
 // Automatically added for the askAwayTab tab
 export * from './askAwayTab/AskAwayTab';
 export * from './askAwayTab/AskAwayTabConfig';
-export * from './askAwayTab/AskAwayTabRemove';
 export * from './askAwayTab/popups/CreateSession';
 export * from './askAwayTab/popups/QnaSessionCreatedNotification';
 export * from './askAwayTab/popups/switch-session/SwitchSession';
