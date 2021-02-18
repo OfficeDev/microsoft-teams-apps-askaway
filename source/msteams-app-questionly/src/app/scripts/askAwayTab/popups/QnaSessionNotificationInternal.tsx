2df200256c88518066bfdac73de804fc37d7ee56
Vamshi Jillala <v-vajill@microsoft.com>
2021-02-17 20:34:34 -0800

    Reserved space for answered button when users names are long

--- source/msteams-app-questionly/src/app/scripts/askAwayTab/popups/QnaSessionNotificationInternal.tsx
+++ source/msteams-app-questionly/src/app/scripts/askAwayTab/popups/QnaSessionNotificationInternal.tsx
@@ -1,6 +1,6 @@
 import './../index.scss';
 import * as React from 'react';
-import { Flex, Text, Button } from '@fluentui/react-northstar';
+import { Flex, Text, Button, FlexItem } from '@fluentui/react-northstar';
 import { useTranslation } from 'react-i18next';
 
 /**
@@ -18,24 +18,21 @@ const QnaSessionNotificationInternal: React.FunctionComponent<QnaSessionNotifica
     const userName = props.searchParams.get('username');
 
     return (
-        <Flex column>
+        <Flex gap="gap.medium" padding="padding.medium" column>
             <Text content={t('popups.notificationTitle', { userName: userName })} />
-            <div className="notification-title">
-                <Text content={sessionTitle} weight="bold" />
-            </div>
-            <Flex gap="gap.large" vAlign="center">
-                <Text content={t('popups.notificationMessage')} />
+            <Text size="medium" content={sessionTitle} weight="bold" />
+            <Text content={t('popups.notificationMessage')} />
+            <FlexItem align="end">
                 <Button
                     primary
                     type="submit"
                     size="small"
+                    content={t('popups.notificationButton')}
                     onClick={() => {
                         props.onSubmitSession();
                     }}
-                >
-                    <Button.Content>{t('popups.notificationButton')}</Button.Content>
-                </Button>
-            </Flex>
+                ></Button>
+            </FlexItem>
         </Flex>
     );
 };
