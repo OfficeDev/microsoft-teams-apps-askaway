ee017f3411612ca908afebe3666d2611e028b82b
Nehal Bhagat <nebhagat@microsoft.com>
2021-02-26 06:24:41 +0000

    Merged PR 2405: Ask away v2: Fixed undefined env variable assignment in constants
    Associated work item https://domoreexp.visualstudio.com/MSTeams/_workitems/edit/1536378
    
    Some constants uses `process.env.variable` for value which might get assigned to undefined if  `dotenvConfig(); // Initialize dotenv, to use .env file settings if existing.` is not finished.
    
    Hence intermittently url: `https://${process.env.HostName}/images/thumbs_up_filled.png` got set to https://urlp.asm.skype.com/v1/url/contenturl=https%3a%2f%2f**undefined**%2fimages%2fthumbs_up_filled.png
    
    This issue won't occur for Prod environment.

--- source/msteams-app-questionly/src/util/keyvault.ts
+++ source/msteams-app-questionly/src/util/keyvault.ts
@@ -4,7 +4,6 @@ import { ifNumber } from 'src/util/typeUtility';
 import { getCredential } from './azureCredentialUtility';
 import { getFromMemoryCache, putIntoMemoryCache } from './memoryCache';
 
-const vaultName = process.env.KeyVaultName;
 const mongoURISecretName = 'MongoDbUri';
 const applicationInsightsInstrumentationKeySecretName = 'ApplicationInsightsInstrumentationKey';
 const microsoftAppPasswordSecretName = 'MicrosoftAppPassword';
@@ -16,6 +15,7 @@ let keyVaultSecretClient: SecretClient;
  * Initialize memory cache and secret client for key vault.
  */
 export const initKeyVault = () => {
+    const vaultName = process.env.KeyVaultName;
     const credential = getCredential();
     const url = `https://${vaultName}.vault.azure.net`;
 
