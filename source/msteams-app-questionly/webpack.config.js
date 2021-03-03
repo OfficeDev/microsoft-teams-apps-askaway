bff16fcd42b7f88cc1c46b348a8986a97ab740a9
Vamshi Jillala <v-vajill@microsoft.com>
2021-03-02 17:11:03 -0800

    removed ar translation.json file

--- source/msteams-app-questionly/webpack.config.js
+++ source/msteams-app-questionly/webpack.config.js
@@ -67,12 +67,8 @@ var config = [
                         to: path.join(__dirname, 'dist/public'),
                     },
                     {
-                        from: path.join(__dirname, 'src/app/localization/locales/en'),
-                        to: path.join(__dirname, 'dist/web/locales/en'),
-                    },
-                    {
-                        from: path.join(__dirname, 'src/app/localization/locales/ar'),
-                        to: path.join(__dirname, 'dist/web/locales/ar'),
+                        from: path.join(__dirname, 'src/app/localization/locales'),
+                        to: path.join(__dirname, 'dist/web/locales'),
                     },
                 ],
             }),
